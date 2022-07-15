f25f82d2e9159aea41b74be4089503041676b209
AbdiAbader <abdiprocode@gmail.com>
2022-07-15 00:31:49 -0700

    print ocal fun update

--- print_oct_unsigned.c
+++ print_oct_unsigned.c
@@ -6,27 +6,29 @@
  */
 int prente_o(va_list args)
 {
-long int decimal, times = 0, x, y, count = 0, num;
-int tmp;
-char s;
-num = va_arg(args, long int);
+int i;
+int *hold;
+int count = 1;
+unsigned int num = va_arg(args, unsigned int);
+unsigned int temp = num, x;
 for (x = num; x > 0; x = x / 8)
-{
-tmp = x % 8;
-times = times * 100 + tmp;
-}
-for (y = times; y > 0; y = y / 100)
-{
-s = y % 100;
-_putchar(s + '0');
 count++;
+hold = malloc(count * sizeof(int));
+for (i = 0; i < count - 1; i++)
+{
+hold[i] = temp % 8;
+temp /= 8;
 }
+count--;
+for (i = count; i >= 0; i--)
+_putchar(hold[i] + '0');
+free(hold);
 return (count);
 }
 /**
- * printf_unsigned - prints integer
- * @args: argument to print
- * Return: number of characters printed
+ *prente_u - prints in oct format
+ *@args: argumenrs
+ *Return: print numbers
  */
 int prente_u(va_list args)
 {
